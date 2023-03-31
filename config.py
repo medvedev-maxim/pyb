edc4d09355a9d98d03d67c2d38d4bfeb89c5810e
MaxiM <medvmax@gmail.com>
2023-03-31 14:52:30 +0300

    pyb-master: TOKEN revoke

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '5533142942:AAGoDtY_KdNbxGOaoT4MyU3sSqRkNZbA9qE'
+TOKEN = '5533142942:AAGoDtY_KdNbxGOaoT4MyU3sSqRkNZbA9qE' #токен не рабочий(перевыпущен)
 
 API_KEI = '2389fda870ad1f93ca7c801610f9b02d'
 
